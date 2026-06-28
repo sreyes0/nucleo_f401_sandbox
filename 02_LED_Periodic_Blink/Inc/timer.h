6f5286be30cf929d15aaac2dbb0d33f2e61349e3
sreyes0 <sreyes.haeusler@gmail.com>
2026-06-28 17:07:17 +0200

    feature(timer): Add base for new timer module

---
+++ 02_LED_Periodic_Blink/Inc/timer.h
@@ -0,0 +1,15 @@
+/*
+ * time.h
+ *
+ * Created on: 28 Jun 2026
+ *     Author: Jimothy
+ *
+ * The time module is an abstraction for the clock of the system to offer an
+ * interface to manage periodic tasks, timeouts, delays, etc.
+ */
+
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+#endif /* TIMER_H_ */
